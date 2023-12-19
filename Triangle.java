da7910ccbd649dcea509675ebcdc6239f52073f4
talsherman70 <tal.sherman@post.runi.ac.il>
2023-12-19 21:20:09 +0200

    add my code to homework1

--- Triangle.java
+++ Triangle.java
@@ -3,8 +3,22 @@
  * This is known as the Triangle Inequality Theorem. 
  * Write a program that tests if three given integers form a triangle.
  */ 
-public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public class Triangle
+{
+    public static void main(String[] args)
+ {
+	 //Recieves 3 integers
+	 int len1 = Integer.parseInt(args[0]);
+	 int len2 = Integer.parseInt(args[1]);
+     int len3 = Integer.parseInt(args[2]);
+	 
+	 boolean ans = false;//boolean that represents the answer whether the sides create a triangle 
+	 
+	 if(len1 + len2 > len3 && len1 + len3 > len2 && len2 + len3 > len1)//checks if the sides can create a triangle
+	 {
+		 ans = true;
+	 }		 
+	 
+	 System.out.println(len1 + ", " + len2 + ", " + len3 + ": " + ans);//prints the sides and the answer
+  }
 }
