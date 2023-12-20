432eb9bc9669cfbb02d2177cb760add01cf8b26c
talsherman70 <tal.sherman@post.runi.ac.il>
2023-12-20 09:47:56 +0200

    add my code to homework1

--- GenThree.java
+++ GenThree.java
@@ -10,16 +10,20 @@ public class GenThree
     int min = Integer.parseInt(args[0]);
 	int max = Integer.parseInt(args[1]);
 	
+	
 	//generates three numbers within the given borders
    	int num1 = (int)(Math.random() * (max - min + 1)) + min;
 	int num2 = (int)(Math.random() * (max - min + 1)) + min;
 	int num3 = (int)(Math.random() * (max - min + 1)) + min;
+	
+	int minNum = Math.min(Math.min(num1, num2), Math.min(num1, num3));//uses the "Math" function to return the minimalnumber of the three numbers
+	
 	 
 	//prints the three random numbers
     System.out.println(num1);
     System.out.println(num2);
 	System.out.println(num3);
 	
-	System.out.println("The minimal generated number was " + Math.min(num1, num2, num3));//Uses Math function to return the minimal number and prints it
+	System.out.println("The minimal generated number was " + minNum);//prints the minimal number
   }
 }
